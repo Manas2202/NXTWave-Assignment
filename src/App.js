631ccb3511787257e9d3b45ff1812272f6a712d8
Manas2202 <70832396+Manas2202@users.noreply.github.com>
2022-10-14 21:56:55 +0530

    first commit
    Created the project and installed all the required dependencies

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import Button from 'react-bootstrap/Button';
+function App() {
+  return (
+    <div className="App">
+      <Button color='primary'>Click me</Button>
+    </div>
+  );
+}
+
+export default App;
